eda3705316173f96d62b5919f82c151d22a66b23
matt <mattjohnson767@gmail.com>
2021-02-04 21:29:58 -0500

    update

--- src/components/card.js
+++ src/components/card.js
@@ -1,13 +1,13 @@
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 
-import '../styles/card.css';
+import "../styles/card.css";
 
-const Card = ({name}) => {
-    return(
-        <div className='pokemon-card'>
-            <link to={`/pokemon/${name}`}>{name}</Link>
-        </div>
-    );
+const Card = ({ name }) => {
+  return (
+    <div className="pokemon-card">
+      <Link to={`/pokemon/${name}`}>{name}</Link>
+    </div>
+  );
 };
 
 export default Card;
